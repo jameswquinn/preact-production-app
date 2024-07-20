63740502d9e27c6bf5c02f81727c43a08858c0b4
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-20 19:36:59 +0100

    Create Footer.js

---
+++ Improved Preact App/src/components/Footer.js
@@ -0,0 +1,10 @@
+// src/components/Footer.js
+import { h } from 'preact';
+
+const Footer = () => (
+  <footer>
+    <p>&copy; 2024 Preact App. All rights reserved.</p>
+  </footer>
+);
+
+export default Footer;
