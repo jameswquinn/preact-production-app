92260b2100e286878880c9c12ec21710a33ef449
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-20 19:33:53 +0100

    Create metaUtils.js

---
+++ Improved Preact App/src/utils/metaUtils.js
@@ -0,0 +1,10 @@
+// src/utils/metaUtils.js
+export function updateMeta(name, content) {
+  let meta = document.querySelector(`meta[name="${name}"]`);
+  if (!meta) {
+    meta = document.createElement('meta');
+    meta.name = name;
+    document.head.appendChild(meta);
+  }
+  meta.content = content;
+}
