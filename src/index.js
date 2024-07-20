d4481b4e80698d564c999705522558b95602fc45
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-20 18:57:39 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import { h, render } from 'preact';
+import App from './App';
+import './styles/main.scss';
+
+render(<App />, document.getElementById('app'));
+
+// Register service worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration);
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError);
+    });
+  });
+}
