92bf8b1673417d7cd956b670abcdd433663cc334
jameswquinn <jameswquinn@users.noreply.github.com>
2024-07-20 19:00:11 +0100

    Create About.jsx

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+import { h } from 'preact';
+
+const About = () => (
+  <div>
+    <h1>About Us</h1>
+    <p>Learn more about our awesome team and project!</p>
+  </div>
+);
+
+export default About;
